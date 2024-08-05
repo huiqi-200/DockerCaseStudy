bf13077be6d7cf9800319b3652d1809265acad18
HQ <61664218+huiqi-200@users.noreply.github.com>
2024-08-05 19:40:01 +0800

    Update top_3_car_manufacturers.sql

--- Task2/Queries/top_3_car_manufacturers.sql
+++ Task2/Queries/top_3_car_manufacturers.sql
@@ -1,3 +1,7 @@
+
+/*
+This query was created during the case study
+*/
 SELECT car_model_dim.manufacturer, COUNT(transaction_fact.transaction_number) as sales_quantity
 FROM transaction_fact 
 RIGHT JOIN car_model_dim 
@@ -5,3 +9,20 @@ ON car_model_dim.car_model_id = transaction_fact.car_model_id
 GROUP BY car_model_dim.manufacturer
 ORDER BY sales_quantity DESC
 LIMIT 3;
+
+/*
+This query was created once I found out about the rank function
+and after i realised it was for current month
+*/
+WITH car_model_rank as (
+  SELECT car_model_dim.manufacturer, COUNT(transaction_fact.transaction_number) as sales_quantity, RANK() OVER ORDER BY sales_quantity as rank
+  FROM transaction_fact 
+  RIGHT JOIN car_model_dim 
+  ON car_model_dim.car_model_id = transaction_fact.car_model_id
+  GROUP BY car_model_dim.manufacturer
+  HAVING MONTH(transaction_fact.date) == MONTH(getdate()) 
+  ORDER BY sales_quantity DESC;
+)
+SELECT car_model_dim, sales_quantity
+FROM car_model_rank
+WHERE rank >= 3;
