f42c2d7b2cc5c906f92b8d539e33a8ea25c30a74
HQ <61664218+huiqi-200@users.noreply.github.com>
2024-08-05 19:41:28 +0800

    Update top_3_car_manufacturers.sql

--- Task2/Queries/top_3_car_manufacturers.sql
+++ Task2/Queries/top_3_car_manufacturers.sql
@@ -15,13 +15,13 @@ This query was created once I found out about the rank function
 and after i realised it was for current month
 */
 WITH car_model_rank as (
-  SELECT car_model_dim.manufacturer, COUNT(transaction_fact.transaction_number) as sales_quantity, RANK() OVER ORDER BY sales_quantity as rank
+  SELECT car_model_dim.manufacturer, COUNT(transaction_fact.transaction_number) as sales_quantity, RANK() OVER (ORDER BY sales_quantity) as rank
   FROM transaction_fact 
   RIGHT JOIN car_model_dim 
   ON car_model_dim.car_model_id = transaction_fact.car_model_id
   GROUP BY car_model_dim.manufacturer
-  HAVING MONTH(transaction_fact.date) == MONTH(getdate()) 
-  ORDER BY sales_quantity DESC;
+  HAVING MONTH(transaction_fact.transaction_datetime) == MONTH(getdate()) 
+  ORDER BY sales_quantity DESC
 )
 SELECT car_model_dim, sales_quantity
 FROM car_model_rank
