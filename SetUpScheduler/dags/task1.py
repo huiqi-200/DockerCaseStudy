5355fd69ec08ffc78a8b64846fe26c8fc38f44ac
HQ <61664218+tinee200@users.noreply.github.com>
2024-08-14 19:15:20 +0800

    Set up airflow scheduler

--- SetUpScheduler/dags/task1.py
+++ SetUpScheduler/dags/task1.py
@@ -1,14 +1,29 @@
+"""
+Adapted from Code that goes along with the Airflow tutorial located at:
+https://github.com/apache/incubator-airflow/blob/master/airflow/example_dags/tutorial.py
+
+
+"""
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+
+
 import pandas as pd
 import os 
 
+
+
+
 def process_dataframe(df):
-
-    # 1. Delete any rows where 'name' is null
-    # reduces number of rows to process in next steps
+    """
+    1. Delete any rows where 'name' is null
+    reduces number of rows to process in next steps
     df = df.dropna(subset=['name'])
-    # 2. Split the 'name' column into two columns 
-    # Assume convention where first_name is in first part of string
-
+    2. Split the 'name' column into two columns 
+    Assume convention where first_name is in first part of string
+    """
     df[['first_name','last_name']] = df['name'].str.split(' ', expand=True)
     
     # 3. Strip any '0's from the 'price' column
@@ -20,7 +35,10 @@ def process_dataframe(df):
     
     return df
 
-if __name__ == "__main__":
+def main():
+    """
+    Gets folder path for all files and performs process_dataframe on them
+    """
     schema = {
     'name': 'string',
     'price': 'string'
@@ -36,3 +54,35 @@ if __name__ == "__main__":
         processed_df = process_dataframe(df)
         processed_df.to_csv(f"{processed_folder_path}/processed_{file}", index=False)
         print(f'file: {file} processed')
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+
+}
+
+with DAG(
+    dag_id="process_dataframe",
+    schedule='* * * * *' ,
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=["pandas_transformation"],
+) as dag:
+    # With the PythonOperator you can run a python function.
+    save_date_task = PythonOperator(
+        task_id='main',
+        python_callable=main,
+        dag=dag
+    )
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Steps:
+Adapted from https://dkangra.medium.com/apache-airflow-install-and-configure-airflow-using-docker-891d1b604556
+
+1. docker compose up -d 
